15fe7b7302e2257be03b000c43639aee19e65473
Chan Jun Yu <chanjunyu1141@gmail.com>
2024-09-22 17:55:14 +0800

    mailer system

---
+++ endpoint/update-user.php
@@ -0,0 +1,56 @@
+<?php
+include ('../conn/conn.php');
+
+$updateUserID = $_POST['user_id'];
+$updateUsername = $_POST['username'];
+$updateEmail = $_POST['email'];
+$updatePassword = $_POST['password'];
+
+try {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash the password
+
+    $conn->beginTransaction();
+
+    // Check if email already exists
+    $stmt = $conn->prepare("SELECT `email` FROM `user` WHERE `email` = :email ");
+    $stmt->execute([
+        'email' => $email
+    ]);
+    $nameExist = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (empty($nameExist)) {
+        $conn->beginTransaction();
+
+        $updateStmt = $conn->prepare("UPDATE `user` SET `username` = :username, `email` = :email,  `password` = :password WHERE `user_id` = :userID");
+        $updateStmt->bindParam(':username', $updateUsername, PDO::PARAM_STR);
+        $updateStmt->bindParam(':email', $updateEmail, PDO::PARAM_STR);
+        $updateStmt->bindParam(':password', $updatePassword, PDO::PARAM_STR);
+        $updateStmt->bindParam(':userID', $updateUserID, PDO::PARAM_INT);
+        $updateStmt->execute();
+
+        echo "
+        <script>
+            alert('Updated Successfully');
+            window.location.href = '../home.php';
+        </script>
+        ";
+
+        $conn->commit();
+    } else {
+        echo "
+        <script>
+            alert('User Already Exist');
+            window.location.href = '../index.php';
+        </script>
+        ";
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+
+
+?>
+
