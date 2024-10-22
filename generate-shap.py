f52b77f45404492fd7a07a1dc3927c3314debb3b
agario8864 <leeenln4@gmail.com>
2024-10-22 22:26:38 +0800

    22/10

--- generate-shap.py
+++ generate-shap.py
@@ -1,22 +1,10 @@
 import matplotlib
-import sys
-import json
 import shap
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 shap.initjs()
-
-# Get the path to the JSON file from PHP
-json_file_path = sys.argv[1]
-
-# Read the JSON data from the file
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-
-# Extract the variables from the data
-variable = data["variable"]
-interaction_variable = data["interaction_variable"]
 
 # Use 'Agg' backend to prevent figures from being displayed
 matplotlib.use('Agg')
@@ -25,14 +13,30 @@ matplotlib.use('Agg')
 shap_values = pd.read_csv("data/shap_values2.csv").to_numpy()
 x_test = pd.read_csv("data/x_test.csv")
 
-# Generate SHAP dependence plot
-fig, ax = plt.subplots(figsize=(6, 4))
-shap.dependence_plot(variable, shap_values, x_test, interaction_index=interaction_variable, ax=ax)
+# List of variables and interaction variables
+variables = ["Log_Inc", "Highest_Certificate", "Saiz_HH", "Age", "Sex", "Strata",
+             "Ethnic_Bumiputera", "Ethnic_Chinese", "Ethnic_Indian", "Ethnic_Others",
+             "Region_Centre", "Region_East", "Region_Eastern Malaysia", "Region_North", "Region_South"]
 
-# Adjust layout
-fig.tight_layout()
+# Directory to save plots
+output_dir = "static/shap_plots/"
+os.makedirs(output_dir, exist_ok=True)
 
-# Save the SHAP plot to a static file
-shap_image_path = "static/shap_plot.png"
-fig.savefig(shap_image_path, bbox_inches='tight')
-plt.close(fig)
+# Generate SHAP dependence plots for all combinations
+for var in variables:
+    for interaction_var in variables:
+        fig, ax = plt.subplots(figsize=(6, 4))
+        
+        if var == interaction_var:
+            # If the variable and interaction variable are the same, exclude interaction_index
+            shap.dependence_plot(var, shap_values, x_test, ax=ax)
+            plot_filename = f"{var}.png"
+        else:
+            # Otherwise, include the interaction_index
+            shap.dependence_plot(var, shap_values, x_test, interaction_index=interaction_var, ax=ax)
+            plot_filename = f"{var}_{interaction_var}.png"
+        
+        fig.tight_layout()
+        plot_path = os.path.join(output_dir, plot_filename)
+        fig.savefig(plot_path, bbox_inches='tight')
+        plt.close(fig)
